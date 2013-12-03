b4693f17936256e586967df11ca0d5e011ee155b
20121BSI0082 <20121BSI0082@SR6713.cefetes.br>
2013-12-03 14:06:57 -0200

    primeiro commit.

---
+++ src/main/java/br/edu/ifes/poo1/CDP/Pecas/Rainha.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifes.poo1.CDP.Pecas;
+
+/**
+ *
+ * @author 20121BSI0082
+ */
+public class Rainha {
+    
+}
---
+++ src/main/java/br/edu/ifes/poo1/xadrez/App.java
@@ -0,0 +1,13 @@
+package br.edu.ifes.poo1.xadrez;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
