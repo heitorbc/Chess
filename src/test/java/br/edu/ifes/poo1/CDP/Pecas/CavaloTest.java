4bc8dddbf7bacc80a5ddeb196dec52a2c896536c
rikes <henrickes.ss@gmail.com>
2014-02-11 11:30:18 -0200

    Voltando aos trabalhos...3

--- src/test/java/br/edu/ifes/poo1/CDP/Pecas/CavaloTest.java
+++ src/test/java/br/edu/ifes/poo1/CDP/Pecas/CavaloTest.java
@@ -6,6 +6,7 @@
 
 package br.edu.ifes.poo1.CDP.Pecas;
 
+import br.edu.ifes.poo1.util.Cor;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -44,20 +45,103 @@ public class CavaloTest {
     @Test
     public void testPodeAndarQuanto() {
         System.out.println("podeAndarQuanto");
-        String posicaoAtual = "";
-        String proximaPosicao = "";
-        Cavalo instance = null;
-        boolean expResult = false;
-        boolean result = instance.podeAndarQuanto(posicaoAtual, proximaPosicao);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String posicaoAtual = "46";
+        String proximaPosicao = "65";
+        boolean expResult = true;
+        boolean result = new Cavalo(Cor.PRETO).podeAndarQuanto(posicaoAtual, proximaPosicao);
+        assertEquals("Espero Verdadeiro",expResult, result);
+        // FAil, espero que ocorra um erro.
+        //fail("The test case is a prototype.");
     }
 
+   @Test
+    public void testPodeAndarQuanto1() {
+        System.out.println("podeAndarQuanto");
+        String posicaoAtual = "46";
+        String proximaPosicao = "27";
+        boolean expResult = true;
+        boolean result = new Cavalo(Cor.PRETO).podeAndarQuanto(posicaoAtual, proximaPosicao);
+        assertEquals("Espero Verdadeiro",expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    @Test
+    public void testPodeAndarQuanto2() {
+        System.out.println("podeAndarQuanto");
+        String posicaoAtual = "46";
+        String proximaPosicao = "25";
+        boolean expResult = true;
+        boolean result = new Cavalo(Cor.PRETO).podeAndarQuanto(posicaoAtual, proximaPosicao);
+        assertEquals("Espero Verdadeiro",expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    @Test
+    public void testPodeAndarQuanto3() {
+        System.out.println("podeAndarQuanto");
+        String posicaoAtual = "46";
+        String proximaPosicao = "38";
+        boolean expResult = true;
+        boolean result = new Cavalo(Cor.PRETO).podeAndarQuanto(posicaoAtual, proximaPosicao);
+        assertEquals("Espero Verdadeiro",expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    @Test
+    public void testPodeAndarQuanto4() {
+        System.out.println("podeAndarQuanto");
+        String posicaoAtual = "46";
+        String proximaPosicao = "67";
+        boolean expResult = true;
+        boolean result = new Cavalo(Cor.PRETO).podeAndarQuanto(posicaoAtual, proximaPosicao);
+        assertEquals("Espero Verdadeiro",expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testPodeAndarQuantoFalse() {
+        System.out.println("podeAndarQuanto");
+        String posicaoAtual = "46";
+        String proximaPosicao = "16";
+        boolean expResult = false;
+        boolean result = new Cavalo(Cor.PRETO).podeAndarQuanto(posicaoAtual, proximaPosicao);
+        assertEquals(" Espero False",expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testPodeAndarQuantoFalse1() {
+        System.out.println("podeAndarQuanto");
+        String posicaoAtual = "46";
+        String proximaPosicao = "56";
+        boolean expResult = false;
+        boolean result = new Cavalo(Cor.PRETO).podeAndarQuanto(posicaoAtual, proximaPosicao);
+        assertEquals(" Espero False",expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /**
      * Test of podeCapturar method, of class Cavalo.
      */
-    @Test
+   
     public void testPodeCapturar() {
         System.out.println("podeCapturar");
         String posicaoAtual = "";
