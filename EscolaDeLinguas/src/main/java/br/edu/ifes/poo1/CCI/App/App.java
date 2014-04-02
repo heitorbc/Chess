a05308d2d6ab49385387fa39f0c37cae1af17bf8
Henrique <henricke.ss@gmail.com>
2014-04-02 00:32:23 -0300

    Manoooo

---
+++ EscolaDeLinguas/src/main/java/br/edu/ifes/poo1/CCI/App/App.java
@@ -0,0 +1,15 @@
+package br.edu.ifes.poo1.CCI.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //System.out.println(Disciplina.ESPANHOL.getNomeDisciplina());
+        //System.out.println(Disciplina.ESPANHOL);
+    }
+}
